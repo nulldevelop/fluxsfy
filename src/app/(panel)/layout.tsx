336d666ffd3f275a1798d5ea0d9157a075946283
devpedroA <pedroandradegt25@gmail.com>
2026-04-24 15:41:56 -0300

    fix: corrigir importação da função getSession em vários componentes

--- src/app/(panel)/layout.tsx
+++ src/app/(panel)/layout.tsx
@@ -5,7 +5,7 @@ import {
   SidebarTrigger,
 } from '@/components/ui/sidebar'
 import { Toaster } from '@/components/ui/sonner'
-import getSession from '@/lib/getSession'
+import { getSession } from '@/lib/getSession'
 import { AppSidebar } from './dashboard/_components/sidebar'
 
 export default async function Layout({
